fc60ff7d7b4e25a918f45ceeaa083f21f35ea05c
Trey0594 <tgonzalezb@ucenfotec.ac.cr>
2026-07-16 19:58:51 -0600

    Definir structs Usuario, Publicacion, Comentario, SolicitudAmistad y Notificacion

--- RedSocialAcademica/RedSocialAcademica/modelos.h
+++ RedSocialAcademica/RedSocialAcademica/modelos.h
@@ -1,1 +1,45 @@
 #pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+// Tipo de usuario 
+enum class TipoUsuario { ESTUDIANTE, PROFESOR, ADMINISTRADOR };
+
+struct Usuario {
+    int id;
+    string nombre;
+    string correo;
+    string carrera;       // vacio si es profesor/admin
+    string institucion;
+    TipoUsuario tipo;
+};
+
+struct Comentario {
+    int id;
+    int idUsuario;
+    string contenido;
+    int fecha;             // formato AAAAMMDD
+};
+
+// Publicacion: la unidad que va a vivir dentro del Arbol B+
+struct Publicacion {
+    int id;
+    int idAutor;
+    string contenido;
+    int fecha;              // AAAAMMDD -> esta es la clave del arbol B+
+    vector<Comentario> comentarios;
+};
+
+struct SolicitudAmistad {
+    int idEmisor;
+    int idReceptor;
+    string estado;          // "pendiente", "aceptada", "rechazada"
+};
+
+struct Notificacion {
+    int id;
+    int idUsuarioDestino;
+    string mensaje;
+    int fecha;
+};
